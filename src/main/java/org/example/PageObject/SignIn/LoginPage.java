ebfc41322e7090122af3f4a7604e3d470dc8fc37
taufikhm58 <taufikhidayatulmunir@gmail.com>
2023-03-29 21:53:41 +0700

    Feature Login

--- src/main/java/org/example/PageObject/SignIn/LoginPage.java
+++ src/main/java/org/example/PageObject/SignIn/LoginPage.java
@@ -1,4 +1,4 @@
-package org.example.PageObject.SignIn;
+package org.example.PageObject;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -6,46 +6,44 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-    public static WebDriver webDriver;
-
-
-    public LoginPage(WebDriver webdriver) {
-        PageFactory.initElements(webdriver, this);
-        this.webDriver = webDriver;
+    public static WebDriver driver;
+    public LoginPage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
     }
 
-    @FindBy(id = "user-name")
-    private WebElement userName;
-
-    @FindBy(xpath = "//input[@id='password']")
-    private WebElement password;
-
-    @FindBy(xpath = "//h3")
-    private WebElement errorMessage;
-
-
-    @FindBy(id = "login-button")
-    private WebElement loginButton;
-
-    public void setUsername(String user) {
-        userName.sendKeys(user);
-    }
-
-    public void setPassword(String pwd) {
-        password.sendKeys(pwd);
-    }
-
-    public void clickLogin() {
-        loginButton.click();
-    }
-
-    public String getErrorMessage() {
-        return errorMessage.getText();
-    }
-    public boolean verifiyLoginPage(){
-        userName.isDisplayed();
+    @FindBy(xpath = "//div[@class='login relative flex flex-col justify-center min-h-screen overflow-hidden']")
+    private WebElement loginPage;
+    public boolean verifyLoginPage(){
+        loginPage.isDisplayed();
         return true;
     }
+
+    @FindBy(xpath = "//form[@class='mt-6 w-full border-b-4 pb-4']/div[@class='mb-5']/input[@class='input input-bordered input-accent w-full max-w-lg pr-10']")
+    private WebElement emailSignIn;
+    public void setEmailSignIn(String email){
+        emailSignIn.sendKeys(email);
+    }
+
+
+    @FindBy(xpath = "//div[@class='relative']/input[@class='input input-bordered input-accent w-full max-w-lg pr-10']")
+    private WebElement passwordSignIn;
+    public void setPasswordSignIn (String pswd){
+        passwordSignIn.sendKeys(pswd);
+    }
+
+    @FindBy(xpath = "//button[@class='rounded-xl bg-lapak w-full max-w-full undefined px-6 py-2 text-[15px] md:text-[15px] lg:text-[14px] 2xl:text-[18px] font-semibold capitalize tracking-wider text-zinc-50 hover:bg-sky-500 disabled:cursor-not-allowed disabled:bg-zinc-400 false']")
+    private WebElement btnLogin;
+    public void setBtnLogin(){
+        btnLogin.click();
+    }
+
+    @FindBy(xpath = "//div[@class='flex flex-col w-11/12 mx-auto items-center']")
+    private WebElement dashboardPage;
+    public boolean verifyDashboardPage(){
+        dashboardPage.isDisplayed();
+        return true;
+    }
+
+
 }
-
-
---
+++ src/test/java/step_definitions/LoginStepDef.java
@@ -0,0 +1,41 @@
+package step_definitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.example.PageObject.LoginPage;
+import org.junit.Assert;
+
+import static step_definitions.Hooks.webDriver;
+
+public class LoginStepDef {
+    @Given("^Open the website LapakUMKM$")
+    public void openTheWebsiteLapakUMKM() {
+        LoginPage loginPage = new LoginPage(webDriver);
+        Assert.assertTrue(loginPage.verifyLoginPage());
+
+    }
+
+    @When("^input email \"([^\"]*)\" and password \"([^\"]*)\"$")
+    public void inputEmailAndPassword(String email, String pswd) {
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.setEmailSignIn(email);
+        loginPage.setPasswordSignIn(pswd);
+    }
+
+    @Then("^click button login$")
+    public void clickButtonLogin() throws InterruptedException {
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.setBtnLogin();
+        Thread.sleep(5000);
+    }
+
+    @And("^already on Dashboard page$")
+    public void alreadyOnDashboardPage() throws InterruptedException {
+        LoginPage loginPage = new LoginPage(webDriver);
+        Assert.assertTrue(loginPage.verifyDashboardPage());
+        Thread.sleep(3000);
+    }
+}
